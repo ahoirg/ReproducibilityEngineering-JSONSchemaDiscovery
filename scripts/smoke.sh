0d962bbf9c7179c99d3b237e553924b7dfb9f92c
Ali Haydar Oezdag <alihaydarozdag@gmail.com>
2024-01-25 00:35:03 +0100

    Update smoke.sh
    Fixes #8 : Updated smoke.sh

--- scripts/smoke.sh
+++ scripts/smoke.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-# Display Node.js version
-echo "Node.js Version:"
-node --version
+success=true
 
-# Display MongoDB version
-echo "MongoDB Version:"
-mongod --version
-
-# Check MongoDB collection
-echo "Checking MongoDB collection..."
-if mongo jsonschemadiscovery --eval "db.getCollectionNames().includes(\"students_orj\")" | grep true; then
-    echo "DB OKAY! Container is ready for use."
+echo "Checking Node.js version..."
+if node -v; then
+    echo "Node.js is OK."
 else
-    echo "DB FAIL"
+    echo "Smoke test failed: There might be an issue with Node.js."
+    success=false
 fi
+
+echo "Checking MongoDB version..."
+if mongod --version; then
+    if mongo jsonschemadiscovery --eval "db.getCollectionNames().includes(\"students_orj\")" | grep true; then
+        echo "Data is ready to use!"
+    else
+        echo "Smoke test failed: There might be an issue with MongoDB."
+        success=false
+    fi
+else
+    echo "Smoke test failed: There might be an issue with MongoDB."
+    success=false
+fi
+
+echo "-------------------------------"
+if $success ; then
+    echo "Smoke test was successful!"
+else
+    echo "Smoke test completed with errors."
+fi
+echo "-------------------------------"
